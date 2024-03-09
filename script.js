a688e08833a2bdd099972c140e5be7d2bd12f1f0
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-03-09 11:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let first = null
+let second = null
+let third = null
+const allInputs = document.querySelectorAll('.toggle')
+allInputs.forEach(input=>{
+	input.addEventListener('click', e=>{
+		if(!first && !second && !third) {
+			first = e.target
+		}	
+		else if(!second && !third){
+			second = e.target
+		}
+		else if(!third) {
+			third = e.target
+			first.checked= false 
+			first = second
+			second = third 
+			third = null		 
+		}
+	})
+})
